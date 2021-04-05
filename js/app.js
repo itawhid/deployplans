a10fc43b44d6af894d24681558edd08648add1c8
Tawhidul Islam <i.tawhid06@gmail.com>
2021-04-05 15:41:08 +0200

    Added Software Component View

--- js/app.js
+++ js/app.js
@@ -27,36 +27,23 @@ loadJSON('data.json',
     }
 );
 
-const buildDeploymentPlans = (deploymentPlansDefault) => {
-  const deploymentPlansWrapper = document.getElementById('deployment-plans-wrapper');
-  const deploymentPlans = deploymentPlansDefault.map((plan, planIndex) => {
-      const deployments = plan.deployments.map((deployment, deploymentIndex) => {
-          let affectedSoftComps = [];
-          for (const log of deployment.device.actionLog) {
-              if (log.action === 'install') {
-                  affectedSoftComps.push(log.affectedSoftwareComponent.externalId);
-              }
-          }
-          affectedSoftComps = [...new Set(affectedSoftComps)];
-
-          const softwareComponents = deployment.softwareComponents.map(softwareComponent => {
-              let status = '';
-              if (affectedSoftComps.includes(softwareComponent.externalId)) {
-                  status = 'checked disabled';
-              }
-
-              return getSoftwareComponentHTML(softwareComponent, status, planIndex, deploymentIndex);
-          });
-
-          return getDeploymentHTML(deployment, softwareComponents);
-      });
-
-      return getPlanHTML(plan, deployments);
-  });
-
-  deploymentPlansWrapper.insertAdjacentHTML('beforeend', deploymentPlans.join(''));
-};
-
+const getSoftwareComponentHTML = (softwareComponent, status, planIndex, deploymentIndex) => {
+  return `<div class="col-6">
+      <div class="card">
+          <div class="card-body">
+              <h4>${softwareComponent.name}</h4>
+              <p>${softwareComponent.version}</p>
+              <div class="form-check">
+                  <input class='form-check-input' type='checkbox' id='check_install_status'
+                      onclick='installComponent(${JSON.stringify(softwareComponent)}, ${planIndex}, ${deploymentIndex})' ${status}>
+                  <label class="form-check-label" for="check_install_status">
+                      Installed
+                  </label>
+              </div>
+          </div>            
+      </div>
+  </div>`;
+}
 
 const getDeploymentHTML = (deployment, softwareComponents) => {
     return `<div class="col-6">
