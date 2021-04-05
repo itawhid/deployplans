902a0ec252ff0a9cf80ed6f3b38630e180a67ebf
Tawhidul Islam <i.tawhid06@gmail.com>
2021-04-05 15:32:32 +0200

    Added deployment view

--- js/app.js
+++ js/app.js
@@ -1,29 +1,45 @@
 let deploymentPlansDefault = [];
 
 const loadJSON = (path, success, error) => {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        if (success) success(JSON.parse(xhr.responseText));
-      } else {
-        if (error) error(xhr);
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+              if (success)
+                  success(JSON.parse(xhr.responseText));
+          } else {
+              if (error)
+                  error(xhr);
+          }
       }
-    }
   };
-  xhr.open('GET', path, true);
+  xhr.open("GET", path, true);
   xhr.send();
 };
 
-loadJSON(
-  'data.json',
-  (data) => {
-    deploymentPlansDefault = data.deploymentPlans;
-    console.log(deploymentPlansDefault);
-  },
-  (xhr) => {
-    console.error(xhr);
-  }
+loadJSON('data.json',
+    data => {
+        console.log(data);
+        deploymentPlansDefault = data.deploymentPlans;
+    },
+    xhr => {
+        console.error(xhr);
+    }
 );
 
 
+const getPlanHTML = (plan, deployments) => {
+    return `<div class="col-12">
+        <div class="card border-success mb-4">
+            <div class="card-header">
+                <h2>${plan.name}</h2>
+                <p>${plan.description}</p>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    ${deployments.join('')}
+                </div>
+            </div>            
+        </div>
+    </div>`;
+}
